799be48443c595f987c690711ef1ebf89458fb15
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-09-11 09:36:50 -0700

    add reactstrap and bootstrap, delete unneeded files

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <KegControl />
+    </React.Fragment>
+  );
+}
+
+export default App;
