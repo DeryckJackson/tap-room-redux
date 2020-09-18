4bd5407cd2b49f287510477caf1e651d6ebb9c08
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-09-18 09:33:23 -0700

    add 2nd failing test for reducer

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -1,6 +1,42 @@
 import kegListReducer from "../../reducers/keg-list-reducer"
 
 describe("kegListReducer", () => {
+
+  let action;
+
+  const kegData = {
+    id: 1,
+    name: "This is only a test Brew",
+    brand: "OnTap",
+    pints: 64,
+    price: 3,
+    alcoholContent: 4
+  }
+
+  test("Should successfully add new keg data to kegList", () => {
+    const { id, name, brand, pints, price, alcoholContent } = kegData;
+
+    action = {
+      type: "ADD_KEG",
+      id,
+      name,
+      brand,
+      pints,
+      price,
+      alcoholContent
+    }
+    expect(kegListReducer({}, action)).toEqual({
+      [id] : {
+        id,
+        name,
+        brand,
+        pints,
+        price,
+        alcoholContent
+      }
+    })
+  })
+
   test("Should return default state if no action type is recognized", () => {
     expect(kegListReducer({}, { type: null })).toEqual({})
   })
--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -1,8 +1,18 @@
 
 
 export default (state = {}, action) => {
-  
+  const { id, name, brand, pints, price, alcoholContent } = action;
   switch (action.type) {
+    case "ADD_KEG":
+      return Object.assign({}, state, {
+        [id]: {
+          // name,
+          // brand,
+          // pints,
+          // price,
+          // alcoholContent
+        }
+      })
     default:
       return state;
   }
