43688bf1b76463cdc280e28401be2d4e8f154f9e
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-09-18 12:57:54 -0700

    add passing tests for root reducer

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -15,4 +15,33 @@ describe("rootReducer", () => {
     });
   })
 
+  test('Check that initial state of kegListReducer matches root reducer', () => {
+    expect(store.getState().kegList).toEqual(kegListReducer(undefined, { type: null }));
+  });
+
+  test('Check that initial state of formVisibleReducer matches root reducer', () => {
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
+  });
+
+  test('Check that initial state of kegListReducer matches root reducer', () => {
+    const action = {
+      type: c.ADD_KEG,
+      id: 1,
+      name: "This is only a test Brew",
+      brand: "OnTap",
+      pints: 64,
+      price: 3,
+      alcoholContent: 4
+    }
+    store.dispatch(action);
+    expect(store.getState().kegList).toEqual(kegListReducer(undefined, action));
+  });
+
+  test('Check that initial state of formVisibleReducer matches root reducer', () => {
+    const action = {
+      type: c.FORM_TOGGLE
+    }
+    store.dispatch(action);
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, action));
+  });
 });
