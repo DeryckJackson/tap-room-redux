f2625d665360e7a79d4af19cb98080932c287ee8
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-09-18 10:31:32 -0700

    add 3rd passing test for new action type ADD_KEG

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,11 +4,11 @@ export const addKeg = (keg) => {
   const { id, name, brand, pints, price, alcoholContent } = keg;
   return {
     type: c.ADD_KEG,
-    // id,
-    // name,
-    // brand,
-    // pints,
-    // price,
-    // alcoholContent
+    id,
+    name,
+    brand,
+    pints,
+    price,
+    alcoholContent
   }
 }
